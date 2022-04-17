1748774627b656460db574c34a3d9430c689b877
Abhixxxtendaction <103671122+Abhixxxtendaction@users.noreply.github.com>
2022-04-17 20:02:56 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption =  f"{file_name} <br> \n @ABHI NEW OTT UPDATES",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '📢our group🔥', url="https://t.me/MOVIE MANIA"
                                 )
                         ]
                     ]
